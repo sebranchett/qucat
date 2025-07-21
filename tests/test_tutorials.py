601f7145c63c4da09b92da668e8c93359428b878
sebranchett <s.e.branchett@tudelft.nl>
2025-07-21 17:33:16 +0200

    which Python versions fail?

--- tests/test_tutorials.py
+++ tests/test_tutorials.py
@@ -18,8 +18,8 @@ class TestAll(TestTutorials):
         var_dict = self.run_tutorial('basics.ipynb')
     def test_filter(self):
         var_dict = self.run_tutorial('filter_design.ipynb')
-    # def test_mmusc(self):
-    #     var_dict = self.run_tutorial('MMUSC.ipynb')
+    def test_mmusc(self):
+        var_dict = self.run_tutorial('MMUSC.ipynb')
     def test_OM(self):
         var_dict = self.run_tutorial('optomechanics.ipynb')
     def test_TC(self):
